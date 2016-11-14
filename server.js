a68447a99f12c32c9be288ecc661a137abbebfb2
Wasif Ateeq <wasifateeq007@hotmail.com>
2016-11-13 19:58:07 -0800

    get all todos page

--- server.js
+++ server.js
@@ -2,10 +2,29 @@ var express=require('express');
 var app=express();
 var PORT=process.env.PORT || 3000;
 
+var todos=[{
+	id:1,description:"Walking the dog",completed:false
+},
+{
+id:2,description:"Jogging",completed:true
+}];
+
+//Root page
 app.get("/",function(req,res) {
 res.send("Hello root page");
 });
 
+//GET /about
+app.get('/about',function(req,res)
+{
+	res.send("About Page");
+})
+
+//Get /todos
+app.get("/todos",function(req,res){
+	res.json(todos);
+
+});
 app.listen(PORT,function(){
 	console.log("Listening on port"+PORT);
 });

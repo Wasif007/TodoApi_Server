a29d8d2518530793db881cca4a617a3241e5b348
Wasif Ateeq <wasifateeq007@hotmail.com>
2016-11-13 20:10:44 -0800

    getting todo item by its id

--- server.js
+++ server.js
@@ -25,6 +25,30 @@ app.get("/todos",function(req,res){
 	res.json(todos);
 
 });
+
+//Get /todos/:id
+app.get("/todos/:id",function(req,res)
+{
+	var todos_id=parseInt(req.params.id);
+	var todo_found;
+	todos.forEach(function(todo)
+		{
+			if(todo.id===todos_id)
+			{
+				todo_found=todo;
+			}
+		});
+	if(todo_found)
+	{
+		res.json(todo_found);
+	}
+	else
+	{
+		res.status(400).send();
+	}
+
+});
+
 app.listen(PORT,function(){
 	console.log("Listening on port"+PORT);
 });

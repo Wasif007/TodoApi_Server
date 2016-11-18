594e0b288380888f0008a6d33a11d3c54fdb175d
Wasif Ateeq <wasifateeq007@hotmail.com>
2016-11-18 01:41:11 -0800

    postgres added for heroku deployment

--- db.js
+++ db.js
@@ -1,8 +1,19 @@
 var Sequelize=require('sequelize');
-var sequelize=new Sequelize(undefined,undefined,undefined,{
+var env_variable=process.env.NODE_ENV || "development";
+var sequelize;
+if(env_variable==="production")
+{
+sequelize=new Sequelize(process.env.DATABASE_URL,{
+dialect:"postgres"
+});
+}
+else{
+sequelize=new Sequelize(undefined,undefined,undefined,{
 "dialect":"sqlite",
 "storage":__dirname+"/data/todosdatabase.sqlite"
-});
+});	
+}
+
 
 var db={};
 db.todo=sequelize.import(__dirname+"/models/todos.js");
